ab9f8ad69a33256f858c0fbe8d72dbfe1e368f76
Priyobrato Das <priyodas12@gmail.com>
2022-04-09 23:50:52 +0530

    initial commit

---
+++ src/main/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplication.java
@@ -0,0 +1,19 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SpringbootJenkinsWarApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJenkinsWarApplication.class, args);
+	}
+
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootJenkinsWarApplication.class);
+    }
+}
---
+++ src/test/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jenkinslab.demoProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
