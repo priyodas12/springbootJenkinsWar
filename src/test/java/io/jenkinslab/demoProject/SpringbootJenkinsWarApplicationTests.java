2f6e5e4426756db529471a537575ca4467e5a9d1
Priyobrato Das <priyodas12@gmail.com>
2022-04-10 01:29:36 +0530

    Update SpringbootJenkinsWarApplicationTests.java

--- src/test/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplicationTests.java
+++ src/test/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplicationTests.java
@@ -1,7 +1,6 @@
 package io.jenkinslab.demoProject;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = io.jenkinslab.demoProject.SpringbootJenkinsWarApplication.class)
