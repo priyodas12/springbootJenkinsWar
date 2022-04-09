f7c43efd42b452e56dc408e9297b7c042cee34d0
Priyobrato Das <priyodas12@gmail.com>
2022-04-10 01:38:56 +0530

    new changes

--- src/main/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplication.java
+++ src/main/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplication.java
@@ -1,4 +1,4 @@
-
+package io.jenkinslab.demoProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplicationTests.java
+++ src/test/java/io/jenkinslab/demoProject/SpringbootJenkinsWarApplicationTests.java
@@ -1,9 +1,10 @@
 package io.jenkinslab.demoProject;
 
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = io.jenkinslab.demoProject.SpringbootJenkinsWarApplication.class)
+@SpringBootTest(classes = SpringbootJenkinsWarApplication.class)
 class SpringbootJenkinsWarApplicationTests {
 
 	@Test
